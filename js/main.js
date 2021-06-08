fe5d5f160639f5b0a8fc83d6bf8fdda52a1b9ab3
Egor Malyshev <egor.mlshv99@mail.ru>
2021-06-09 00:39:22 +0300

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // Функция, генерирующая случайное число в заданном интервале
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getCoordinates = (a, b) => {
-  let result = Math.random() * (b - a) + a;
-  if (result >= 0) {
-    return result;
+const getRandomNumber = (min, max, num) => {
+  if (min > max) {
+    return console.log('Первое число больше второго');
+  } else if (min < 0 || max < 0 || num < 0) {
+    return console.log('Значение меньше нуля');
   }
-  return alert("Введено неверное значение");
-}
+  const earlyNumber = (Math.floor(Math.random() * (max - min + 1)) + min);
+  const finalNumber = earlyNumber.toFixed(num);
+  return console.log(`Результат: + ${finalNumber}`);
+};
+
+getRandomNumber(1, 5, 4);
