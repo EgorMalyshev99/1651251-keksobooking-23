aee86e6dc3c550ad0e8ce318404cf95279c6e3cf
EgorMalyshev99 <egor.mlshv99@mail.ru>
2021-06-14 20:41:40 +0300

    final version1.0

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Функция, генерирующая случайное число в заданном интервале
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomNumber = (min, max, num) => {
-  if (min > max || [...arguments].some(item => item < 0)) {
+  if (min > max || min < 0 || max < 0 || num < 0) {
     return false;
     // return console.log("error");
   }
