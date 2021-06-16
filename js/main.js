e9f0bd607dbff6f599a6d826557786cab12ae9fd
Egor Malyshev <egor.mlshv99@mail.ru>
2021-06-16 11:34:56 +0300

    alpha version

--- js/main.js
+++ js/main.js
@@ -14,56 +14,93 @@ const getRandomNumber = (min, max, num) => {
 
 // Задание 2
 
-import { getRandomPositiveInteger } from "./utils/get-random-positive-integer.js";
+import {
+  getRandomPositiveInteger
+} from "./utils/get-random-positive-integer.js";
 
-const createArr = (count, ttl, building) => {
-  let array = new Array; // Создаем массив для хранения JS-объектов
+const createHotels = (count) => {
 
-  if (count > 10 || count <= 0) { // Проверяем на неверно введенные значения
-    // return false;
-    return alert("Введено неверное число");
-  } else if (String(count).length === 1) { // Добавляем "0" спереди, если число от 1 до 9
-    count = ("0" + count);
+  // *****Data*****
+
+  let location = {
+    lat: getRandomNumber(35.65, 35.7, 5),
+    lng: getRandomNumber(139.7, 139.8, 5)
+  };
+  let buildings = ["palace", "flat", "house", "bungalow", "hotel"];
+  let checkTime = ["12:00", "13:00", "14:00"];
+  let typeOfRoom = ["standart", "comfort", "luxe", "president"];
+  let facilities = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+  let photoExamples = [
+    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
+    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
+    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg"
+  ]
+
+  // *****Functions*****
+
+  // Вывод случайных элементов массива
+  const createRandomItems = (arr) => {
+    let newArr = [];
+    let rand = getRandomPositiveInteger(1, 6);
+
+    for (let i = 0;
+      (i < rand) && (i < arr.length); i++) {
+      let r = Math.floor(Math.random() * (arr.length - i)) + i;
+      newArr[i] = arr[r];
+      arr[r] = arr[i];
+      arr[i] = newArr[i];
+    }
+
+    return newArr;
   }
 
-  if (building !== "palace" && building !== "flat" && building !== "house" && building !== "bungalow" && building !== "hotel") {
-    // return false;
-    return alert("Введено невеное значение");
+  // Возврат случайного элемента из массива
+  const getOneOfArr = (arr) => {
+    let items;
+    return items = arr[getRandomPositiveInteger(0, arr.length - 1)];
   }
+
+  // Функия для генерации массива случайной длинны из значений передаваемого массива
+  const createPhotos = (arr) => {
+    let newArr = [];
+    let randomLength = getRandomPositiveInteger(1, 10);
+
+    for (let i = 0; i <= randomLength; i++) {
+      newArr[i] = arr[getRandomPositiveInteger(0, arr.length - 1)]
+    }
+
+    return newArr;
+  }
+
+  // *****Элементы итогового объекта*****
 
   let author = {
     avatar: `img/avatars/user${count}.png`
-  }
-
-  let location = {
-    lat: getRandomNumber(1, 10, 6),
-    lng: getRandomNumber(1, 10, 6)
-  }
-
-  const createFeatures = () => {
-    let arr = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-    let numberOfValues = getRandomPositiveInteger(0, 5);
-    return arr[numberOfValues];
-  }
+  };
 
   let offer = {
-    title: ttl,
-    adress: `${location.lat}` + ', ' + `${location.lng}`, // Warn
-    price: getRandomPositiveInteger(0, 1000000), // Warn
-    type: building,
-    rooms: getRandomPositiveInteger(0, 10), // Warn
-    guests: getRandomPositiveInteger(0, 10), // Warn
-    checkin: "12:00",
-    ckeckout: "14:00",
-    features: createFeatures
+    title: `Отель ${count+1}`,
+    adress: `${location.lat}` + ', ' + `${location.lng}`,
+    price: getRandomPositiveInteger(1000, 1000000), // !Warn!
+    type: getOneOfArr(buildings),
+    rooms: getRandomPositiveInteger(1, 10), // !Warn!
+    guests: getRandomPositiveInteger(1, 10), // !Warn!
+    checkin: getOneOfArr(checkTime),
+    ckeckout: getOneOfArr(checkTime),
+    features: createRandomItems(facilities),
+    description: getOneOfArr(typeOfRoom),
+    photos: createPhotos(photoExamples)
   }
 
-  array = {
+  let hotel = {
     author,
     offer
   }
 
-  return console.log(array);
+  return console.log(hotel); // Вывод объекта в консоль
 }
 
-createArr(5, "zozozo", "flat");
+// Генерация отелей
+for (let i = 0; i < 10; i++) {
+  createHotels(i);
+}
