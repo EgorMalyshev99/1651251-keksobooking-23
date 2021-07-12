2fcd13bb9ccd20d4a89ee4e43155947701467739
Egor Malyshev <egor.mlshv99@mail.ru>
2021-07-13 02:01:11 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -8,16 +8,21 @@ import {
   setActive,
   setDisabled
 } from './utils/work-state.js';
+import {
+  setValidForm
+} from './utils/work-with-form.js';
 
 const hotels = [];
 
-setDisabled();
-setTimeout(setActive, 3000);
+// setDisabled();
+// setTimeout(setActive, 3000);
 
 for (let index = 0; index < 10; index++) {
   hotels[index] = createHotel(index);
 }
 
-hotels.forEach((hotel) => {
-  createPopup(hotel);
-});
+// hotels.forEach((hotel) => {
+//   createPopup(hotel);
+// });
+
+setValidForm();
---
+++ js/utils/work-with-form.js
@@ -0,0 +1,52 @@
+const adForm = document.querySelector('.ad-form');
+const title = document.querySelector('#title');
+const apsType = document.querySelector('#type');
+const appsPrice = document.querySelector('#price');
+const submitBtn = document.querySelector('.ad-form__submit');
+
+export const setValidForm = () => {
+
+  adForm.setAttribute('method', 'POST');
+  adForm.setAttribute('action', 'https://23.javascript.pages.academy/keksobooking');
+  adForm.setAttribute('enctype', 'multipart/form-data');
+
+  apsType.addEventListener('change', (event) => {
+    switch (apsType.options[apsType.selectedIndex].value) {
+      case 'flat':
+        appsPrice.setAttribute('min', 1000);
+        appsPrice.setAttribute('placeholder', 1000);
+        break;
+      case 'bungalow':
+        appsPrice.setAttribute('min', 0);
+        appsPrice.setAttribute('placeholder', 0);
+        break;
+      case 'house':
+        appsPrice.setAttribute('min', 5000);
+        appsPrice.setAttribute('placeholder', 5000);
+        break;
+      case 'palace':
+        appsPrice.setAttribute('min', 10000);
+        appsPrice.setAttribute('placeholder', 10000);
+        break;
+      case 'hotel':
+        appsPrice.setAttribute('min', 3000);
+        appsPrice.setAttribute('placeholder', 3000);
+        break;
+      default:
+        break;
+    }
+  });
+
+  title.addEventListener('change', () => {
+    if (title.validity.tooShort) {
+      title.classList.add('error-border');
+      title.setCustomValidity('Заголовок должен состоять минимум из 5-ти символов');
+    } else if (title.validity.tooLong) {
+      title.setCustomValidity('Заголовок не должен превышать 100 символов');
+    } else if (title.validity.valueMissing) {
+      title.setCustomValidity('Обязательное поле');
+    } else {
+      title.setCustomValidity('');
+    }
+  });
+}
